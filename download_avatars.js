eed852eb74374963621fda59eff52d811c8e269a
FredLavoie <flavoie3@gmail.com>
2019-06-04 00:32:28 +0000

    Add comments to clarify code

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,15 @@
+// list of modules required
 const request = require('request');
 const fs = require('fs');
 const token = require('./secrets.js');
+
+// get user input values for repo name and repo owner
 const repoName = process.argv[2];
 const repoOwner = process.argv[3];
 
 
 let getRepoContributors = function(owner, name, callback) {
-  
+
   let options = {
     url: 'https://api.github.com/repos/' + owner + '/' + name + '/contributors',
     headers: { 
@@ -14,26 +17,27 @@ let getRepoContributors = function(owner, name, callback) {
       'Authorization': `token ${token.GITHUB_TOKEN}`,
     }
   };
-    
+  // request contributors JSON data from github api    
   request(options, function(error, response, body) {
     console.log('error: ', error);
     console.log('statusCode: ', response && response.statusCode);
             
     let obj = JSON.parse(body);
-    callback(obj);
+    callback(obj);  // call anonymous function of getRepoContributors function
   });
   
 };
 
 getRepoContributors(repoOwner, repoName, function(obj) {
 
+  // cycle through contributors to get each of their avatars
   for (let i = 0; i < obj.length; i++) {
     let url = obj[i]['avatar_url'];
     let filePath = `./avatars/${obj[i]['login']}.jpg`;
 
 
-    let downloadImageByURL = function(url, filePath) {
- 
+    let downloadImageByURL = function(url, filePath) { 
+      // request individual avatars and save them to file
       request(url)
         .on('error', function(err) {
           throw err;
